82e444774e238092866bc367b3f428c12bbc8256
AmatsuZero <jzh16s@hotmail.com>
2020-03-15 16:23:10 +0800

    ADD: prepare local server side

---
+++ cli-showcase/server/main.ts
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+const server = app.listen(1234, () => {
+    const host = server.address().address
+    const port = server.address().port
+
+    console.log('应用实例，访问地址为 http://%s:%s', host, port)
+})
