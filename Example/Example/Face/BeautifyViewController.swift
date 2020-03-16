0ade1a5d619e6ef506215d3f10891b86eb460ce2
姜振华 <jzh16s@hotmail.com>
2020-03-16 21:01:13 +0800

    ADD：Example 工程增加嵌套 .swiftlint.yml

---
+++ Example/Example/Face/BeautifyViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BeautifyViewController.swift
+//  Example
+//
+//  Created by 姜振华 on 2020/3/16.
+//  Copyright © 2020 FaceppSwift. All rights reserved.
+//
+
+import UIKit
+
+class BeautifyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
