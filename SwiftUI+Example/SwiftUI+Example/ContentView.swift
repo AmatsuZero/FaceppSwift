a94a7e1e85633e7e2d4494845536dc9ac208db6b
姜振华 <jzh16s@hotmail.com>
2020-03-23 10:47:02 +0800

    ADD: Init SwiftUI Example + subspec

---
+++ SwiftUI+Example/SwiftUI+Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI+Example
+//
+//  Created by 姜振华 on 2020/3/23.
+//  Copyright © 2020 facepp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
